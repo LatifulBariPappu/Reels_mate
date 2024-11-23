0d6ddebd793055a3c500e6b48f68f97bc74b7038
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-11-24 00:02:04 +0600

    signup layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.reelsmate"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.reelsmate"
--- app/src/main/java/com/example/reelsmate/SignupActivity.kt
+++ app/src/main/java/com/example/reelsmate/SignupActivity.kt
@@ -3,9 +3,9 @@ package com.example.reelsmate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
