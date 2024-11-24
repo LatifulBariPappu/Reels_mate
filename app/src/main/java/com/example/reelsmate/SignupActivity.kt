7832f341f1249e74fd2156a608662a95d5f04554
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-11-24 23:21:30 +0600

    field validity

--- app/src/main/java/com/example/reelsmate/SignupActivity.kt
+++ app/src/main/java/com/example/reelsmate/SignupActivity.kt
@@ -1,11 +1,44 @@
 package com.example.reelsmate
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
+import com.example.reelsmate.databinding.ActivitySignupBinding
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitBtn.setOnClickListener {
+            signup()
+        }
+    }
+    fun signup(){
+        val email = binding.emailInput.text.toString()
+        val passsword = binding.passwordInput.text.toString()
+        val confirmPassword = binding.confirmPasswordInput.text.toString()
+
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailInput.setError("Email not valid")
+            return
+        }
+        if(passsword.length<6){
+            binding.passwordInput.setError("Minimum 6 character")
+            return
+        }
+        if(confirmPassword.length<6){
+            binding.confirmPasswordInput.setError("Password not matched")
+            return
+        }
+        signupwithFirebasse()
+    }
+    fun signupwithFirebasse(){
+
     }
 }
