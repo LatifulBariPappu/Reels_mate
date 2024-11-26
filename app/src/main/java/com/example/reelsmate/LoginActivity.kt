679d3c95302c65cdc1e16490355aae618c55bf9e
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-11-27 01:14:34 +0600

    login success

--- app/src/main/java/com/example/reelsmate/LoginActivity.kt
+++ app/src/main/java/com/example/reelsmate/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.reelsmate
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.reelsmate.databinding.ActivityLoginBinding
+import com.example.reelsmate.util.UiUtil
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,10 +18,53 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.submitBtn.setOnClickListener {
+            login()
+        }
 
         binding.goToSignupBtn.setOnClickListener {
             startActivity(Intent(this,SignupActivity::class.java))
             finish()
+        }
+
+    }
+    fun setInProgress(inProgress : Boolean){
+        if(inProgress){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.submitBtn.visibility = View.GONE
+        }else{
+            binding.progressBar.visibility = View.GONE
+            binding.submitBtn.visibility = View.VISIBLE
+        }
+    }
+
+    fun login() {
+        val email = binding.emailInput.text.toString()
+        val passsword = binding.passwordInput.text.toString()
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailInput.setError("Email not valid")
+            return
+        }
+        if(passsword.length<6) {
+            binding.passwordInput.setError("Minimum 6 character")
+            return
+        }
+        loginwithFirebasse(email,passsword)
+
+    }
+
+    fun loginwithFirebasse(email: String, passsword: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            email,passsword
+        ).addOnSuccessListener {
+            UiUtil.showToast(this,"Login Successfully")
+            setInProgress(false)
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }.addOnFailureListener {
+            UiUtil.showToast(applicationContext,it.localizedMessage?:"Something went wrong")
+            setInProgress(false)
         }
     }
 }
